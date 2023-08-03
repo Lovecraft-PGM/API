a5ba463b1482d39e2a388a048cde983de395f166
Diego Castro <diegofer40520@gmail.com>
2023-08-03 18:56:16 -0500

    update3
    probado validacion

--- app/Http/Controllers/ServiceController.php
+++ app/Http/Controllers/ServiceController.php
@@ -6,21 +6,20 @@ use Illuminate\Http\Request;
 
 class ServiceController extends Controller
 {
-    static function frontendResponse($status, $type ,$data = null,$msgError = null) {
+    static function frontendResponse($status, $type, $data = null, $msgError = null)
+    {
 
-        $response =[
-            'status' =>$status,
-            'type' =>$type
+        $response = [
+            'status' => $status,
+            'type' => $type
         ];
         if (!is_null($data))
             $response['data'] = $data;
-
-        if($response['type']== 'error'){
-            if(!is_null($msgError))
-            $response['error'] = ['message' => $msgError]; 
+            
+        if ($response['type'] == 'error' Or $data == null) {
+            if (!is_null($msgError))
+                $response['error'] = ['message' => $msgError];
         }
-        return response()-> json($response);   
-    } 
-
-   
+        return response()->json($response);
+    }
 }
